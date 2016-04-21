50459676a569f7adbb40c2c4170891647a5c9ce2
biehu <276620879@qq.com>
2016-04-21 18:13:06 +0800

    blog

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
+import Nav from './Nav';
+
+import '../statics/css/app.css';
+
+@connect(state => ({
+    loading: state.routerState.fetching 
+}))
+export default class App extends React.Component {
+	static propTypes = {
+        loading: PropTypes.bool,
+        children: PropTypes.element
+    }
+	
+	render() {
+		var {children, loading} = this.props;
+		return (
+			<div className="wrap">
+				<Nav />
+				{loading ? '加载中...' : children}
+			</div>
+		);
+	}
+}
